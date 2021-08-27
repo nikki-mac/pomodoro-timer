5c36708209deb6b901e30da3903bf80e09bc21eb
Nicole Macdonald <85309505+nikki-mac@users.noreply.github.com>
2021-08-27 13:59:41 -0700

    Adding Comments

--- src/pomodoro/TimerControls.js
+++ src/pomodoro/TimerControls.js
@@ -10,19 +10,23 @@ function TimerControls({ timerControlsHandler, time, sessionType, disabled }) {
          <div className="col">
            <div className="input-group input-group-lg mb-2">
              <span className="input-group-text" data-testid="duration-focus">
+             {/* TODO: Update this text to display the current focus session duration */}
                Focus Duration: {minutesToDuration(time[0])}
              </span>
+
              <div className="input-group-append">
+             {/* TODO: Implement decreasing focus duration and disable during a focus session */}
                <button
                  type="button"
                  className="btn btn-secondary"
                  data-testid={`decrease-${sessionType[0]}`}
                  onClick={() => timerControlsHandler('minus', sessionType[0])}
                  disabled={disabled}
-
                >
                  <span className="oi oi-minus" />
                </button>
+               
+              {/* TODO: Implement increasing focus duration and disable during a focus session */}
                <button
                  type="button"
                  className="btn btn-secondary"
@@ -43,7 +47,9 @@ function TimerControls({ timerControlsHandler, time, sessionType, disabled }) {
                <span className="input-group-text" data-testid="duration-break">
                  Break Duration: {minutesToDuration(time[1])}
                </span>
+               
                <div className="input-group-append">
+               {/* TODO: Implement decreasing focus duration and disable during a break session */}
                  <button
                    type="button"
                    className="btn btn-secondary"
@@ -53,6 +59,8 @@ function TimerControls({ timerControlsHandler, time, sessionType, disabled }) {
                  >
                    <span className="oi oi-minus" />
                  </button>
+                 
+                {/* TODO: Implement increasing focus duration and disable during a break session */}
                  <button
                    type="button"
                    className="btn btn-secondary"
